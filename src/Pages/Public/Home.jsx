4d7a3971b891e9db2ab2fd7630ac45e6be01d547
azhakarasan3549 <alagualagu189@gmail.com>
2026-04-05 14:33:01 +0530

    to bg overlay  test

--- src/Pages/Public/Home.jsx
+++ src/Pages/Public/Home.jsx
@@ -11,23 +11,32 @@ const Home = () => {
   
   return (
     < >
-     <div
-  className="bg-cover bg-center bg-no-repeat"
-style={{ backgroundImage: "url('/background.svg')" }}
+   <div
+  className="relative bg-cover bg-center bg-no-repeat"
+  style={{ backgroundImage: "url('/background.svg')" }}
 >
-      <Navbar />
-      {/* Hero */}
-      <section className="min-h-[70vh] flex items-center">
-        <div className="max-w-4xl mx-auto text-center px-4">
-          <h1 className="text-3xl md:text-4xl font-bold mb-4">
-            Find Your Life Partner
-          </h1>
-          <p className="text-gray-400 mb-6">
-            Trusted matrimonial service with verified profiles.
-          </p>
-        </div>
-      </section>
+  {/* ✅ Overlay */}
+  <div
+    className="absolute   bg-no-repeat bg-[length:1500px] inset-0 bg-center opacity-5"
+    style={{ backgroundImage: "url('/design.svg')" }}
+  ></div>
+
+  {/* ✅ Content */}
+  <div className="relative z-10">
+    <Navbar />
+
+    <section className="min-h-[50vh] flex items-center">
+      <div className="max-w-4xl mx-auto text-center px-4">
+        <h1 className="text-3xl md:text-4xl font-bold mb-4">
+          Find Your Life Partner
+        </h1>
+        <p className="text-gray-400 mb-6">
+          Trusted matrimonial service with verified profiles.
+        </p>
       </div>
+    </section>
+  </div>
+</div>
 
       <Howitswork />
       <Carousel/>
