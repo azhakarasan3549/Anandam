3aea836e43a87a1f1a955a24f6d1d9ccaf8d2787
azhakarasan3549 <alagualagu189@gmail.com>
2026-04-20 22:21:37 +0530

    to fix_1 Hero section

--- src/Pages/Public/Home.jsx
+++ src/Pages/Public/Home.jsx
@@ -5,6 +5,8 @@ import { useContext } from "react";
 import { UserProfiles} from "../../Context/UserContext.jsx"
 import Carousel from "../../Components/Carousel.jsx";
 import Cast from "../../Components/Cast.jsx";
+import { HeroSection } from "./HeroSection.jsx";
+
 
 const Home = () => {
   const { profiles, loading } = useContext(UserProfiles); 
@@ -24,17 +26,8 @@ const Home = () => {
   {/* ✅ Content */}
   <div className="relative z-10">
     <Navbar />
+    <HeroSection/>
 
-    <section className="min-h-[50vh] md:min-h-[60vh]  flex items-center">
-      <div className="max-w-4xl mx-auto text-center px-4">
-        <h1 className="text-3xl md:text-4xl font-bold mb-4">
-          Find Your Life Partner
-        </h1>
-        <p className="text-gray-400 mb-6">
-          Trusted matrimonial service with verified profiles.
-        </p>
-      </div>
-    </section>
   </div>
 </div>
       <Cast/>
